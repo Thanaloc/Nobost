ff47af210c2735d951615b03529e0848857353dc
Thanaloc <raphael.seguin.97@gmail.com>
2026-02-16 14:28:52 +0100

    [Add] inpout handling

--- Assets/_Project/Scripts/Player/PlayerInputHandler.cs
+++ Assets/_Project/Scripts/Player/PlayerInputHandler.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    [SerializeField] private InputActionAsset _InputActions;
+    private InputSystem_Actions _inputActions;
 
     public Vector2 MoveInput { get; private set; }
     public Vector2 LookInput { get; private set; }
@@ -15,4 +15,41 @@ public class PlayerInputHandler : MonoBehaviour
     // OnEnable : activer et s'abonner
     // OnDisable : désabonner et désactiver
     // les callbacks mettent à jour les properties
+
+    private void Awake()
+    {
+        _inputActions = new();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+
+        //Move
+        _inputActions.Player.Move.performed += context => MoveInput = context.ReadValue<Vector2>();
+        _inputActions.Player.Move.canceled += context => MoveInput = Vector2.zero;
+
+        //Look
+        _inputActions.Player.Look.performed += context => LookInput = context.ReadValue<Vector2>();
+        _inputActions.Player.Look.canceled += context => LookInput = Vector2.zero;
+
+        //sprint
+        _inputActions.Player.Sprint.performed += context => SprintPressed = true;
+        _inputActions.Player.Sprint.canceled += context => SprintPressed = false;
+
+        //crouch
+        _inputActions.Player.Crouch.performed += context => CrouchPressed = true;
+        _inputActions.Player.Crouch.canceled += context => CrouchPressed = false;
+
+        //Jump
+        _inputActions.Player.Jump.performed += context => JumpPressed = true;
+        _inputActions.Player.Jump.canceled += context => JumpPressed = false;
+
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Player.Disable();
+    }
+
 }
