a9b28abae4507754d8ab4914084424690a748146
Thanaloc <raphael.seguin.97@gmail.com>
2026-02-17 15:08:27 +0100

    [Fix] bugs & names

--- Assets/_Project/Scripts/AI/AIPatrol.cs
+++ Assets/_Project/Scripts/AI/AIPatrol.cs
@@ -6,55 +6,37 @@ public class AIPatrol : MonoBehaviour
 {
     [SerializeField] private Transform[] _Waypoints;
 
-    [SerializeField] private AiSettingsSO _AIConfig;
+    [SerializeField] private AISettingsSO _AIConfig;
 
     [SerializeField] private NavMeshAgent _Agent;
 
     private IEnumerator _agentCoroutine;
-    private bool _restartCoroutine = false;
 
     private void Start()
     {
+        _Agent.speed = _AIConfig.PatrolSpeed;
         _agentCoroutine = AgentMoveRoutine();
         StartCoroutine(_agentCoroutine);
     }
 
-    private void Update()
-    {
-        if (_restartCoroutine)
-        {
-            StopCoroutine(_agentCoroutine);
-            _agentCoroutine = null;
-            _agentCoroutine = AgentMoveRoutine();
-            StartCoroutine( _agentCoroutine);
-            _restartCoroutine = false;
-        }
-    }
 
     private IEnumerator AgentMoveRoutine()
     {
         int i = 0;
-        while (i < _Waypoints.Length)
+        while (true)
         {
             _Agent.SetDestination(_Waypoints[i].position);
 
-            if (_Agent.pathPending)
+            yield return null;
+
+            while (_Agent.pathPending || _Agent.remainingDistance > 0.2f)
                 yield return null;
 
-            while (_Agent.remainingDistance > 0)
-            {
-                yield return new WaitForEndOfFrame();
-            }
+            yield return new WaitForSeconds(_AIConfig.WaitTime);
 
-            if (_Agent.remainingDistance == 0)
-            {
-                yield return new WaitForSeconds(_AIConfig.WaitTime);
-            }
-
-            i++;
+            i = (i + 1) % _Waypoints.Length;
         }
 
-        _restartCoroutine = true;
 
     }
 }
--- Assets/_Project/Scripts/AI/AiSettingsSO.cs
+++ Assets/_Project/Scripts/AI/AiSettingsSO.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Data/AI/AISettings")]
-public class AiSettingsSO : ScriptableObject
+public class AISettingsSO : ScriptableObject
 {
     public float PatrolSpeed = 4.0f;
     public float WaitTime = 3.0f;
